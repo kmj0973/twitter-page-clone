e3c57b0ce839ef2a513ae691fb9744ab88291cd4
kmj0973 <kmj0973@naver.com>
2023-06-18 05:22:05 +0900

    Update login Form, firebase Auth
    Update login Form, firebase Auth

--- twitter_page_clone/src/component/App.js
+++ twitter_page_clone/src/component/App.js
@@ -1,12 +1,12 @@
-import AppRouter from "./Router";
-import React, { useState } from "react";
-import { authService } from "../myBase";
+import AppRouter from './Router';
+import React, { useState } from 'react';
+import { authService } from '../myBase';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-  const user = authService.currentUser;
-  console.log(user);
-  return <AppRouter isLoggedIn={isLoggedIn} />;
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const user = authService.currentUser;
+    console.log(user);
+    return <AppRouter isLoggedIn={isLoggedIn} />;
 }
 
 export default App;
