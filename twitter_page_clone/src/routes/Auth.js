815d4038f8f7e960b0cddd147fc61351f86242bc
kmj0973 <kmj0973@naver.com>
2023-06-19 03:27:27 +0900

    Update login with firebase
    Update login with firebase

--- twitter_page_clone/src/routes/Auth.js
+++ twitter_page_clone/src/routes/Auth.js
@@ -1,41 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { authService } from "../myBase";
 
 const Auth = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    const onChange = (event) => {
-        const { name, value } = event.target;
-        if (name === 'email') {
-            setEmail(value);
-        } else if (name === 'password') {
-            setPassword(value);
-        }
-        console.log(event.target.name);
-    };
-    const onSubmit = (event) => {
-        event.preventDefault();
-    };
-    return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
-                <input
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                    required
-                    vlaue={password}
-                    onChange={onChange}
-                />
-                <input type="submit" value="Log In" />
-            </form>
-            <div>
-                <button>Continue with Google</button>
-                <button>Continue with Github</button>
-            </div>
-        </div>
-    );
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+    console.log(event.target.name);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      const auth = getAuth();
+      if (newAccount) {
+        data = await createUserWithEmailAndPassword(auth, email, password);
+      } else {
+        data = await signInWithEmailAndPassword(auth, email, password);
+      }
+      console.log(data);
+      console.log(authService.currentUser);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          vlaue={password}
+          onChange={onChange}
+        />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
