1e85b688d3e83fd208ba592f362e06753fa8136e
kmj0973 <kmj0973@naver.com>
2023-06-20 02:13:38 +0900

    Update Social Login, google, github
    Update Social Login, google, github

--- twitter_page_clone/src/component/App.js
+++ twitter_page_clone/src/component/App.js
@@ -1,12 +1,24 @@
 import AppRouter from "./Router";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { authService } from "../myBase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
-  const user = authService.currentUser;
-  console.log(user);
-  return <AppRouter isLoggedIn={isLoggedIn} />;
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  console.log(authService.currentUser);
+  return (
+    <>{init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}</>
+  );
 }
 
 export default App;
--- twitter_page_clone/src/routes/Auth.js
+++ twitter_page_clone/src/routes/Auth.js
@@ -3,6 +3,9 @@ import {
   getAuth,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
 } from "firebase/auth";
 import { authService } from "../myBase";
 
@@ -10,6 +13,7 @@ const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
   const onChange = (event) => {
     const { name, value } = event.target;
     if (name === "email") {
@@ -31,6 +35,22 @@ const Auth = () => {
       }
       console.log(data);
       console.log(authService.currentUser);
+    } catch (error) {
+      setError(error.message.replace("Firebase:", ""));
+    }
+  };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (event) => {
+    console.log(event.target.name);
+    let provider;
+    try {
+      if (event.target.name === "google") {
+        provider = new GoogleAuthProvider();
+        const result = await signInWithPopup(authService, provider);
+      } else if (event.target.name === "github") {
+        provider = new GithubAuthProvider();
+        const result = await signInWithPopup(authService, provider);
+      }
     } catch (error) {
       console.log(error);
     }
@@ -56,10 +76,18 @@ const Auth = () => {
         />
         <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign In" : "Create Account"}
+      </span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with Github
+        </button>
       </div>
+      {error}
     </div>
   );
 };
