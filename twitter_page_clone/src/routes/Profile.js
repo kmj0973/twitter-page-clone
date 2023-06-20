8fff2b56fe242f8941cbf72b06da38793fbe736a
kmj0973 <kmj0973@naver.com>
2023-06-20 17:23:05 +0900

    Update Sign out
    Update Sign out

--- twitter_page_clone/src/routes/Profile.js
+++ twitter_page_clone/src/routes/Profile.js
@@ -1,3 +1,20 @@
-import React from "react";
-
-export default () => <span>profile</span>;
+import React from 'react';
+import { authService } from '../myBase';
+import { signOut } from 'firebase/auth';
+import { Link } from 'react-router-dom';
+export default () => {
+    const onLogOutClick = () => {
+        try {
+            signOut(authService);
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+    return (
+        <>
+            <button onClick={onLogOutClick} name="Logout">
+                Logout
+            </button>
+        </>
+    );
+};
