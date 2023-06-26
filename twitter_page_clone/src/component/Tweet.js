a8731c9d4645962aeb1f8efd7b01467ac0b8de7e
kmj0973 <kmj0973@naver.com>
2023-06-26 22:20:16 +0900

    Update delete photo from storage, store
    Update delete photo from storage, store

--- twitter_page_clone/src/component/Tweet.js
+++ twitter_page_clone/src/component/Tweet.js
@@ -1,70 +1,68 @@
-import React, { useState } from "react";
-import { doc, deleteDoc, updateDoc } from "firebase/firestore";
-import { getStorage, ref, deleteObject } from "firebase/storage";
-import { dbService } from "../myBase";
+import React, { useState } from 'react';
+import { doc, deleteDoc, updateDoc } from 'firebase/firestore';
+import { getStorage, ref, deleteObject } from 'firebase/storage';
+import { dbService } from '../myBase';
 
 const Tweet = ({ tweetObj, isOwner }) => {
-  const [editing, setEditing] = useState(false);
-  const [newTweet, setNewTweet] = useState(tweetObj.text);
-  const onDeleteClick = async (event) => {
-    const ok = window.confirm("Are you sure you want to delete this tweet?");
-    console.log(ok);
-    if (ok) {
-      await deleteDoc(doc(dbService, "tweets", `${tweetObj.id}`));
-      if (tweetObj.fileUrl) {
-        await deleteObject(ref(getStorage(), tweetObj.fileUrl));
-      }
-    }
-  };
-  const toggleEditing = () => {
-    setEditing((prev) => !prev);
-  };
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    await updateDoc(doc(dbService, "tweets", `${tweetObj.id}`), {
-      text: newTweet,
-    });
-    setEditing(false);
-  };
-  const onChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setNewTweet(value);
-  };
-  return (
-    <div>
-      {editing ? (
-        <>
-          <form onSubmit={onSubmit}>
-            <input
-              type="text"
-              placeholder="Edit your Tweet"
-              value={newTweet}
-              required
-              onChange={onChange}
-            />
-            <input type="file" accept="image/*" />
-            <input type="submit" value="Update Tweet" />
-          </form>
-          <button onClick={toggleEditing}>Cancel</button>
-        </>
-      ) : (
-        <>
-          {tweetObj.fileUrl && (
-            <img src={tweetObj.fileUrl} width="100px" height="100px" />
-          )}
-          <h3>{tweetObj.text} </h3>
-          {isOwner && (
-            <>
-              <button onClick={onDeleteClick}>Delete Tweet</button>
-              <button onClick={toggleEditing}>Edit Tweet</button>
-            </>
-          )}
-        </>
-      )}
-    </div>
-  );
+    const [editing, setEditing] = useState(false);
+    const [newTweet, setNewTweet] = useState(tweetObj.text);
+    const onDeleteClick = async (event) => {
+        const ok = window.confirm('Are you sure you want to delete this tweet?');
+        console.log(ok);
+        if (ok) {
+            await deleteDoc(doc(dbService, 'tweets', `${tweetObj.id}`));
+            if (tweetObj.fileUrl) {
+                await deleteObject(ref(getStorage(), tweetObj.fileUrl));
+            }
+        }
+    };
+    const toggleEditing = () => {
+        setEditing((prev) => !prev);
+    };
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await updateDoc(doc(dbService, 'tweets', `${tweetObj.id}`), {
+            text: newTweet,
+        });
+        setEditing(false);
+    };
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNewTweet(value);
+    };
+    return (
+        <div>
+            {editing ? (
+                <>
+                    <form onSubmit={onSubmit}>
+                        <input
+                            type="text"
+                            placeholder="Edit your Tweet"
+                            value={newTweet}
+                            required
+                            onChange={onChange}
+                        />
+                        <input type="file" accept="image/*" />
+                        <input type="submit" value="Update Tweet" />
+                    </form>
+                    <button onClick={toggleEditing}>Cancel</button>
+                </>
+            ) : (
+                <>
+                    {tweetObj.fileUrl && <img src={tweetObj.fileUrl} width="100px" height="100px" />}
+                    <h3>{tweetObj.text} </h3>
+                    {isOwner && (
+                        <>
+                            <button onClick={onDeleteClick}>Delete Tweet</button>
+                            <button onClick={toggleEditing}>Edit Tweet</button>
+                        </>
+                    )}
+                </>
+            )}
+        </div>
+    );
 };
 
 export default Tweet;
--- twitter_page_clone/src/routes/Profile.js
+++ twitter_page_clone/src/routes/Profile.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import { authService } from '../myBase';
-import { signOut } from 'firebase/auth';
-import { Link } from 'react-router-dom';
-export default () => {
+import React, { useEffect, useState } from 'react';
+import { authService, dbService } from '../myBase';
+import { signOut, updateProfile } from 'firebase/auth';
+import { collection, where, getDocs, query, orderBy } from 'firebase/firestore';
+
+export default ({ userObj }) => {
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+
     const onLogOutClick = () => {
         try {
             signOut(authService);
@@ -10,8 +13,41 @@ export default () => {
             console.log(error.message);
         }
     };
+    const getMyTweets = async () => {
+        const q = query(
+            collection(dbService, 'tweets'),
+            where('creatorId', '==', `${userObj.uid}`),
+            orderBy('createdAt', 'desc')
+        );
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, ' => ', doc.data());
+        });
+    };
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if (userObj.displayName !== newDisplayName) {
+            await updateProfile(userObj, {
+                displayName: newDisplayName,
+            });
+        }
+    };
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNewDisplayName(value);
+    };
+    useEffect(() => {
+        getMyTweets();
+    }, []);
     return (
         <>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange} value={newDisplayName} type="text" placeholder="Display Name" />
+                <input type="submit" placeholder="Update Profile" />
+            </form>
             <button onClick={onLogOutClick} name="Logout">
                 Logout
             </button>
