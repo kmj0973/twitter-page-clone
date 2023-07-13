1566272d65ade36f855f900ef5128dfb482ac4aa
kmj0973 <kmj0973@naver.com>
2023-07-14 05:46:30 +0900

    Complete tweet js, edit and delete tweet
    Complete tweet js, edit and delete tweet

--- twitter_page_clone/src/component/Navigation.js
+++ twitter_page_clone/src/component/Navigation.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import twitter_icon from '../img/twitter-icon.png';
-import user_icon from '../img/user-icon.png';
-import menu_icon from '../img/menu-icon.png';
-import DialogDemo from './DialogDemo';
+import React from "react";
+import { Link } from "react-router-dom";
+import twitter_icon from "../img/twitter-icon.png";
+import user_icon from "../img/user-icon.png";
+import menu_icon from "../img/menu-icon.png";
+import DialogDemo from "./TweetDialog";
 const Navigation = ({ userObj }) => {
-    const onClick = () => {};
-    return (
-        <nav className="navigation">
-            <ul>
-                <li>
-                    <button type="click">
-                        <img src={menu_icon} alt="menu" width="30px" />
-                    </button>
-                </li>
-                <li>
-                    <Link to="/">
-                        <img src={twitter_icon} alt="twitter" width="30px" />
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/profile">
-                        <img src={user_icon} alt="user" width="30px" />
-                        {/* {userObj.displayName}의 My profile */}
-                    </Link>
-                </li>
-            </ul>
-        </nav>
-    );
+  const onClick = () => {};
+  return (
+    <nav className="navigation">
+      <ul>
+        <li>
+          <button type="click">
+            <img src={menu_icon} alt="menu" width="30px" />
+          </button>
+        </li>
+        <li>
+          <Link to="/">
+            <img src={twitter_icon} alt="twitter" width="30px" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/profile">
+            <img src={user_icon} alt="user" width="30px" />
+            {/* {userObj.displayName}의 My profile */}
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navigation;
--- twitter_page_clone/src/routes/Home.js
+++ twitter_page_clone/src/routes/Home.js
@@ -1,111 +1,126 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { authService, dbService, storageService } from '../myBase';
-import { ref, uploadString, getDownloadURL } from 'firebase/storage';
-import { v4 as uuidv4 } from 'uuid';
-import Tweet from '../component/Tweet';
-import DialogDemo from '../component/DialogDemo';
-import { collection, addDoc, getDocs, onSnapshot, query, orderBy } from 'firebase/firestore';
-import { onAuthStateChanged } from 'firebase/auth';
+import React, { useEffect, useState, useRef } from "react";
+import { authService, dbService, storageService } from "../myBase";
+import { ref, uploadString, getDownloadURL } from "firebase/storage";
+import { v4 as uuidv4 } from "uuid";
+import Tweet from "../component/Tweet";
+import DialogDemo from "../component/TweetDialog";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  onSnapshot,
+  query,
+  orderBy,
+} from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
 
 const Home = ({ userObj }) => {
-    const [tweet, setTweet] = useState('');
-    const [tweets, setTweets] = useState([]);
-    const [fileAttach, setFileAttach] = useState('');
-    const fileInput = useRef();
-    useEffect(() => {
-        const q = query(collection(dbService, 'tweets'), orderBy('createdAt', 'desc'));
-        onSnapshot(q, (snapshot) => {
-            const TweetArr = snapshot.docs.map((doc) => {
-                return { id: doc.id, ...doc.data() };
-            });
-            setTweets(TweetArr);
-        });
-        console.log(userObj);
-        // onAuthStateChanged(authService, (user) => {
-        //   if (user != null) {
-        //     unsubscribe();
-        //   }
-        // });
-    }, []);
-    const onSubmit = async (event) => {
-        event.preventDefault();
-        let fileUrl = '';
-        if (tweet === '') return window.confirm('Write your mind');
-        if (fileAttach != '') {
-            const fileRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
-            const response = await uploadString(fileRef, fileAttach, 'data_url');
-            fileUrl = await getDownloadURL(response.ref);
-        }
-        const tweetObj = {
-            text: tweet,
-            createdAt: Date.now(),
-            creatorId: userObj.uid,
-            displayName: userObj.displayName,
-            fileUrl,
-        };
-        await addDoc(collection(dbService, 'tweets'), tweetObj);
-        setTweet('');
-        setFileAttach('');
-        fileInput.current.value = null;
-    };
-    const onChange = (event) => {
-        const {
-            target: { value },
-        } = event;
-        setTweet(value);
-    };
-    const onFileChange = (event) => {
-        console.log(event);
-        const {
-            target: { files },
-        } = event;
-        const theFile = files[0];
-        const reader = new FileReader();
-        reader.onloadend = (finishedEvent) => {
-            const {
-                currentTarget: { result },
-            } = finishedEvent;
-            setFileAttach(result);
-        };
-        if (theFile) {
-            reader.readAsDataURL(theFile);
-        }
-    };
-    const onClear = () => {
-        fileInput.current.value = null;
-        setFileAttach('');
-    };
-    return (
-        <div className="main-home">
-            <form onSubmit={onSubmit} className="tweet-form__input">
-                <input
-                    value={tweet}
-                    onChange={onChange}
-                    type="text"
-                    placeholder="What's on your mind?"
-                    maxLength={120}
-                />
-                <input type="file" accept="image/*" onChange={onFileChange} ref={fileInput} />
-                {fileAttach && (
-                    <div>
-                        <img src={fileAttach} width="50px" height="50px" />
-                        <button onClick={onClear}>Clear</button>
-                    </div>
-                )}
-                <input type="submit" value="Tweet" />
-            </form>
-            <DialogDemo />
-            <div className="tweet-form">
-                {tweets.map((tweet) => (
-                    <Tweet
-                        key={tweet.id}
-                        userObj={userObj}
-                        tweetObj={tweet}
-                        isOwner={tweet.creatorId === userObj.uid}
-                    />
-                ))}
-            </div>
-        </div>
+  const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]);
+  const [fileAttach, setFileAttach] = useState("");
+  const fileInput = useRef();
+  useEffect(() => {
+    const q = query(
+      collection(dbService, "tweets"),
+      orderBy("createdAt", "desc")
     );
+    onSnapshot(q, (snapshot) => {
+      const TweetArr = snapshot.docs.map((doc) => {
+        return { id: doc.id, ...doc.data() };
+      });
+      setTweets(TweetArr);
+    });
+    console.log(userObj);
+    // onAuthStateChanged(authService, (user) => {
+    //   if (user != null) {
+    //     unsubscribe();
+    //   }
+    // });
+  }, []);
+  //   const onSubmit = async (event) => {
+  //     event.preventDefault();
+  //     let fileUrl = "";
+  //     if (tweet === "") return window.confirm("Write your mind");
+  //     if (fileAttach != "") {
+  //       const fileRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
+  //       const response = await uploadString(fileRef, fileAttach, "data_url");
+  //       fileUrl = await getDownloadURL(response.ref);
+  //     }
+  //     const tweetObj = {
+  //       text: tweet,
+  //       createdAt: Date.now(),
+  //       creatorId: userObj.uid,
+  //       displayName: userObj.displayName,
+  //       fileUrl,
+  //     };
+  //     await addDoc(collection(dbService, "tweets"), tweetObj);
+  //     setTweet("");
+  //     setFileAttach("");
+  //     fileInput.current.value = null;
+  //   };
+  //   const onChange = (event) => {
+  //     const {
+  //       target: { value },
+  //     } = event;
+  //     setTweet(value);
+  //   };
+  //   const onFileChange = (event) => {
+  //     console.log(event);
+  //     const {
+  //       target: { files },
+  //     } = event;
+  //     const theFile = files[0];
+  //     const reader = new FileReader();
+  //     reader.onloadend = (finishedEvent) => {
+  //       const {
+  //         currentTarget: { result },
+  //       } = finishedEvent;
+  //       setFileAttach(result);
+  //     };
+  //     if (theFile) {
+  //       reader.readAsDataURL(theFile);
+  //     }
+  //   };
+  //   const onClear = () => {
+  //     fileInput.current.value = null;
+  //     setFileAttach("");
+  //   };
+  return (
+    <div className="main-home">
+      {/* <form onSubmit={onSubmit} className="tweet-form__input">
+        <input
+          value={tweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input
+          type="file"
+          accept="image/*"
+          onChange={onFileChange}
+          ref={fileInput}
+        />
+        {fileAttach && (
+          <div>
+            <img src={fileAttach} width="50px" height="50px" />
+            <button onClick={onClear}>Clear</button>
+          </div>
+        )}
+        <input type="submit" value="Tweet" />
+      </form> */}
+      <DialogDemo userObj={userObj} />
+      <div className="tweet-form">
+        {tweets.map((tweet) => (
+          <Tweet
+            key={tweet.id}
+            userObj={userObj}
+            tweetObj={tweet}
+            isOwner={tweet.creatorId === userObj.uid}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Home;
